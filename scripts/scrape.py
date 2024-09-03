19337b65a893552eaa6dc6aaaa61c1cbe922a875
ulizuli <82661499+ulizuli@users.noreply.github.com>
2024-09-03 22:41:29 +1000

    creating parse notebook

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -20,77 +20,82 @@ from urllib.request import urlopen, Request
 BASE_URL = "https://www.domain.com.au"
 N_PAGES = range(1, 5) # update this to your liking
 
-# begin code
-url_links = []
-property_metadata = defaultdict(dict)
+def start_scrape():
 
-# generate list of urls to visit
-for page in N_PAGES:
-    url = BASE_URL + f"/rent/melbourne-region-vic/?sort=price-desc&page={page}"
-    print(f"Visiting {url}")
-    bs_object = BeautifulSoup(urlopen(Request(url, headers={'User-Agent':"PostmanRuntime/7.6.0"})), "lxml")
+    # begin code
+    url_links = []
+    property_metadata = defaultdict(dict)
 
-    # find the unordered list (ul) elements which are the results, then
-    # find all href (a) tags that are from the base_url website.
-    index_links = bs_object \
-        .find(
-            "ul",
-            {"data-testid": "results"}
-        ) \
-        .findAll(
-            "a",
-            href=re.compile(f"{BASE_URL}/*") # the `*` denotes wildcard any
-        )
+    # generate list of urls to visit
+    for page in N_PAGES:
+        url = BASE_URL + f"/rent/melbourne-region-vic/?sort=price-desc&page={page}"
+        print(f"Visiting {url}")
+        bs_object = BeautifulSoup(urlopen(Request(url, headers={'User-Agent':"PostmanRuntime/7.6.0"})), "lxml")
 
-    for link in index_links:
-        # if its a property address, add it to the list
-        if 'address' in link['class']:
-            url_links.append(link['href'])
+        # find the unordered list (ul) elements which are the results, then
+        # find all href (a) tags that are from the base_url website.
+        index_links = bs_object \
+            .find(
+                "ul",
+                {"data-testid": "results"}
+            ) \
+            .findAll(
+                "a",
+                href=re.compile(f"{BASE_URL}/*") # the `*` denotes wildcard any
+            )
 
-# for each url, scrape some basic metadata
-pbar = tqdm(url_links[1:])
-success_count, total_count = 0, 0
-for property_url in pbar:
-    bs_object = BeautifulSoup(urlopen(Request(property_url, headers={'User-Agent':"PostmanRuntime/7.6.0"})), "lxml")
-    total_count += 1
-    
-    try: 
-        # looks for the header class to get property name
-        property_metadata[property_url]['name'] = bs_object \
-            .find("h1", {"class": "css-164r41r"}) \
-            .text
+        for link in index_links:
+            # if its a property address, add it to the list
+            if 'address' in link['class']:
+                url_links.append(link['href'])
 
-        # looks for the div containing a summary title for cost
-        property_metadata[property_url]['cost_text'] = bs_object \
-            .find("div", {"data-testid": "listing-details__summary-title"}) \
-            .text
-
-        # get rooms and parking
-        rooms = bs_object \
-                .find("div", {"data-testid": "property-features"}) \
-                .findAll("span", {"data-testid": "property-features-text-container"})
-
-        # rooms
-        property_metadata[property_url]['rooms'] = [
-            re.findall(r'\d+\s[A-Za-z]+', feature.text)[0] for feature in rooms
-            if 'Bed' in feature.text or 'Bath' in feature.text
-        ]
-        # parking
-        property_metadata[property_url]['parking'] = [
-            re.findall(r'\S+\s[A-Za-z]+', feature.text)[0] for feature in rooms
-            if 'Parking' in feature.text
-        ]
-
-        property_metadata[property_url]['desc'] = re \
-            .sub(r'<br\/>', '\n', str(bs_object.find("p"))) \
-            .strip('</p>')
-        success_count += 1
+    # for each url, scrape some basic metadata
+    pbar = tqdm(url_links[1:])
+    success_count, total_count = 0, 0
+    for property_url in pbar:
+        bs_object = BeautifulSoup(urlopen(Request(property_url, headers={'User-Agent':"PostmanRuntime/7.6.0"})), "lxml")
+        total_count += 1
         
-    except AttributeError:
-        print(f"Issue with {property_url}")
+        try: 
+            # looks for the header class to get property name
+            property_metadata[property_url]['name'] = bs_object \
+                .find("h1", {"class": "css-164r41r"}) \
+                .text
 
-    pbar.set_description(f"{(success_count/total_count * 100):.0f}% successful")
+            # looks for the div containing a summary title for cost
+            property_metadata[property_url]['cost_text'] = bs_object \
+                .find("div", {"data-testid": "listing-details__summary-title"}) \
+                .text
 
-# output to example json in data/raw/
-with open('../data/raw/example.json', 'w') as f:
-    dump(property_metadata, f)
+            # get rooms and parking
+            rooms = bs_object \
+                    .find("div", {"data-testid": "property-features"}) \
+                    .findAll("span", {"data-testid": "property-features-text-container"})
+
+            # rooms
+            property_metadata[property_url]['rooms'] = [
+                re.findall(r'\d+\s[A-Za-z]+', feature.text)[0] for feature in rooms
+                if 'Bed' in feature.text or 'Bath' in feature.text
+            ]
+            # parking
+            property_metadata[property_url]['parking'] = [
+                re.findall(r'\S+\s[A-Za-z]+', feature.text)[0] for feature in rooms
+                if 'Parking' in feature.text
+            ]
+
+            property_metadata[property_url]['desc'] = re \
+                .sub(r'<br\/>', '\n', str(bs_object.find("p"))) \
+                .strip('</p>')
+            success_count += 1
+            
+        except AttributeError:
+            print(f"Issue with {property_url}")
+
+        pbar.set_description(f"{(success_count/total_count * 100):.0f}% successful")
+
+    # output to example json in data/raw/
+    with open('../data/raw/example.json', 'w') as f:
+        dump(property_metadata, f)
+    
+
+
